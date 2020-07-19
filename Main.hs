97d6a517ae0bd1cdd34ba33445572bd82325ac9c
Meowcolm024 <luoyicong1026@live.com>
2020-07-19 11:17:24 +0800

    preparing loop

--- Main.hs
+++ Main.hs
@@ -17,6 +17,8 @@ main = print $ map toBfOp <$> regularParse
     parseExpr
     (getVal "++++++++[>++++++<-]>+.")
 
+eval x = map toBfOp <$> regularParse parseExpr x
+
 -- Parser
 
 regularParse :: Parser a -> String -> Either ParseError a
@@ -29,7 +31,7 @@ parseAtom :: Parser (Bf String)
 parseAtom = Atom <$> (many1 . oneOf) "+-<>."
 
 parseExpr :: Parser [Bf String]
-parseExpr = many $ choice [try parseLoop, parseAtom]
+parseExpr = many1 $ choice [try parseLoop, parseAtom]
 
 -- Helper
 
@@ -55,8 +57,23 @@ act :: Op -> Act
 act = \case
     Mr -> Right (\(Pointer p m) -> Pointer (p+1) m)
     Ml -> Right (\(Pointer p m) -> Pointer (p-1) m)
-    Ci -> Right (\(Pointer p m) -> Pointer p (f p m))
-    Cr -> Right (\(Pointer p m) -> Pointer p (f p m))
+    Ci -> Right (\(Pointer p m) -> Pointer p (f p (+) m))
+    Cr -> Right (\(Pointer p m) -> Pointer p (f p (-) m))
     Pr -> Left (\(Pointer p m) -> putChar (toEnum (m !! p) :: Char))
-  where f a xs = let (lp, rp) = splitAt a xs in lp ++ [head rp +1] ++ tail rp
+  where f a op xs = let (lp, rp) = splitAt a xs in lp ++ [head rp `op` 1] ++ tail rp
 
+walk :: Pointer -> Bf [Op] -> IO Pointer
+walk pt (Atom ops) = chainF rs pt
+    where
+        rs = map act ops
+        fx :: Act -> Pointer -> IO Pointer
+        fx (Right x) acc = return $ x acc
+        fx (Left x) acc = x acc >> return acc
+        chainF :: [Act] -> Pointer -> IO Pointer
+        chainF [] x = return x
+        chainF (f:fs) x = fx f x >>= chainF fs
+walk pt@(Pointer 0 _) (Loop _) = return pt
+
+walkBf :: [Bf [Op]] -> Pointer -> IO Pointer
+walkBf [] x = return x
+walkBf (b:bs) x = walk x b >>= walkBf bs
